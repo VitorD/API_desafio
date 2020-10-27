a18d98aad6cbaa0cc17ede959a583ccd4d13c7bd
João Vitor Daltro <31117770+VitorD@users.noreply.github.com>
2020-10-27 11:26:01 -0300

    criaçao do projeto iniciada

---
+++ API_desafio/Controllers/ViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API_desafio.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "api rest de usuarios";
+            return View();
+        }
+    }
+}
---
+++ API_desafio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_desafio.WebApiApplication" Language="C#" %>
